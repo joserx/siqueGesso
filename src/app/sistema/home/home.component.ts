442180d8c66f836a801ad38de437acb7494e1a58
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-05-05 18:26:16 -0300

    updated home page (dynamic cards)

--- src/app/sistema/home/home.component.ts
+++ src/app/sistema/home/home.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public cards = [
+    { nome: "Vendas", icon: "bi-shop", href: "" },
+    { nome: "Compras", icon: "bi-cart4", href: "" },
+    { nome: "Estoque", icon: "bi-box-seam", href: "" },
+    { nome: "Financeiro", icon: "bi-file-earmark-bar-graph", href: "" },
+    { nome: "Expedição", icon: "bi-truck", href: "" },
+    { nome: "RH", icon: "bi-person-lines-fill", href: "" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
