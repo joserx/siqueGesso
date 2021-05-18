622df4cce7f24266d4d056d32578df91503bdffc
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-05-18 14:12:33 -0300

    updated notificacoes config page

--- src/app/sistema/notificacoes/notificacoes.component.ts
+++ src/app/sistema/notificacoes/notificacoes.component.ts
@@ -8,18 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class NotificacoesComponent implements OnInit {
 
 
-  public notificacoes = [
-    { nome: "Notificação 1", value: "notificacao1", tipos: [
+  public notificacoesEmail = [
+    { nome: "Notificação 1", value: "notificacaoEmail1", tipos: [
       { nome: "Tipo 1" },
       { nome: "Tipo 2" },
       { nome: "Tipo 3" },
     ]},
-    { nome: "Notificação 2", value: "notificacao2", tipos: [
+    { nome: "Notificação 2", value: "notificacaoEmail2", tipos: [
       { nome: "Tipo 1" },
       { nome: "Tipo 2" },
       { nome: "Tipo 3" },
     ]},
-    { nome: "Notificação 3", value: "notificacao3", tipos: [
+    { nome: "Notificação 3", value: "notificacaoEmail3", tipos: [
+      { nome: "Tipo 1" },
+      { nome: "Tipo 2" },
+      { nome: "Tipo 3" },
+    ]},
+  ]
+
+  public notificacoesSique = [
+    { nome: "Notificação 1", value: "notificacaoSique1", tipos: [
+      { nome: "Tipo 1" },
+      { nome: "Tipo 2" },
+      { nome: "Tipo 3" },
+    ]},
+    { nome: "Notificação 2", value: "notificacaoSique2", tipos: [
+      { nome: "Tipo 1" },
+      { nome: "Tipo 2" },
+      { nome: "Tipo 3" },
+    ]},
+    { nome: "Notificação 3", value: "notificacaoSique3", tipos: [
       { nome: "Tipo 1" },
       { nome: "Tipo 2" },
       { nome: "Tipo 3" },
