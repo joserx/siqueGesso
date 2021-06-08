c97263732e7a2414854158b6ae582ff639231966
brunafinger <bruna.grassi@fingerdigital.com.br>
2021-06-08 15:02:05 -0300

    tabela expedição ok

--- src/app/sistema/expedicao/expedicao.component.ts
+++ src/app/sistema/expedicao/expedicao.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpedicaoComponent implements OnInit {
 
+  public ordens = [
+    { dataCriacao: '05/05/2021', numOrdem: "001", pedido: "001", dataDespacho: "06/05/2021", status: "Aguardando" },
+    { dataCriacao: '10/05/2021', numOrdem: "002", pedido: "003", dataDespacho: "10/05/2021", status: "Aguardando"},
+    { dataCriacao: '15/05/2021', numOrdem: "003", pedido: "004", dataDespacho: "15/05/2021", status: "Aguardando"},
+    { dataCriacao: '21/05/2021', numOrdem: "004", pedido: "005", dataDespacho: "22/05/2021", status: "Aguardando" },
+    { dataCriacao: '23/05/2021', numOrdem: "005", pedido: "006", dataDespacho: "23/05/2021", status: "Aguardando"},
+    { dataCriacao: '25/05/2021', numOrdem: "006", pedido: "007", dataDespacho: "25/05/2021", status: "Aguardando"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
