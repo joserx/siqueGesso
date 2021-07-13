ea4544bee00981675111a009c7110c161de3c24c
felipegomes163 <felipe.gomes@fingerdigital.com.br>
2021-07-13 16:21:58 -0300

    CRUD User find

--- src/app/sistema/usuarios-sistema/listar-usuarios-sistema/listar-usuarios-sistema.component.ts
+++ src/app/sistema/usuarios-sistema/listar-usuarios-sistema/listar-usuarios-sistema.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioSistemaService } from 'src/app/services/usuario-sistema.service';
 
 @Component({
   selector: 'app-listar-usuarios-sistema',
   templateUrl: './listar-usuarios-sistema.component.html',
-  styleUrls: ['./listar-usuarios-sistema.component.scss']
+  styleUrls: ['./listar-usuarios-sistema.component.scss'],
 })
 export class ListarUsuariosSistemaComponent implements OnInit {
 
-  public usuarios = [
-    { id: 1, nome: 'Michael', sobrenome: 'B. Jordan', email: 'michael.jordan@siquegesso.com', permissao: 'Administrador' },
-    { id: 2, nome: 'Ricardo', sobrenome: 'Botega', email: 'ricardo.botega@siquegesso.com', permissao: 'Administrador' },
-    { id: 3, nome: 'Thais', sobrenome: 'Camila', email: 'thais.camila@siquegesso.com', permissao: 'Administrador' },
-    { id: 4, nome: 'Deise', sobrenome: 'Teixeira', email: 'deise.teixeira@siquegesso.com', permissao: 'Administrador' },
-    { id: 5, nome: 'Henrique', sobrenome: 'Bustillos', email: 'deise.teixeira@siquegesso.com', permissao: 'Administrador' },
-  ];
+  usuarios : any;
 
-  constructor() { }
+  constructor(
+    private readonly usuarioSistemasService : UsuarioSistemaService
+  ) { }
 
   ngOnInit(): void {
+    this.usuarioSistemasService.find().subscribe((data : any) => {
+      this.usuarios = data
+    })
+  }
+
+  deleteUser(id : number) {
+    this.usuarioSistemasService.delete(id).subscribe((data : any) => {
+      this.usuarios = this.usuarios.filter((ele : any) => { return ele.id != id })
+    })
   }
 
 }
