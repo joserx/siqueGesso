b44ef4116a8f8f88bbe72ee91bbd51008f5f3253
felipegomes163 <felipe.gomes@fingerdigital.com.br>
2021-08-10 15:47:29 -0300

    Add new clients

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  find() {
+    return this.http.get(environment.apiUrl + 'client/');
+  }
+
+  findOne(id: number) {
+    return this.http.get(environment.apiUrl + 'client/' + id);
+  }
+
+  data() {
+    return this.http.get(environment.apiUrl + 'client/data');
+  }
+
+  create(data: any) {
+    return this.http.post(environment.apiUrl + 'client', data);
+  }
+
+  update(id: number, data: any) {
+    return this.http.put(environment.apiUrl + 'client/' + id, data);
+  }
+
+  delete(id: number) {
+    return this.http.delete(environment.apiUrl + 'client/' + id);
+  }
+
+}
--- src/app/sistema/vendas/clientes/cadastrar-cliente/cadastrar-cliente.component.ts
+++ src/app/sistema/vendas/clientes/cadastrar-cliente/cadastrar-cliente.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
+import { CorreiosService } from 'src/app/services/correios.service';
+import { BrazilValidator } from 'src/app/_helpers/brasil';
 import { getDate } from 'src/environments/global';
 
 @Component({
@@ -13,35 +17,60 @@ export class CadastrarClienteComponent implements OnInit {
   public desativadoCheckbox: boolean = false;
   public tipoPessoa: string = 'fisica';
   clienteForm : FormGroup = new FormGroup({
-    'name' : new FormControl(null),
-    'surname' : new FormControl(null),
-    'cpf' : new FormControl(null),
-    'rg' : new FormControl(null),
-    'cellphone' : new FormControl(null),
-    'telephone' : new FormControl(null),
-    'birthDate' : new FormControl(null),
+    'name' : new FormControl(null, [Validators.required]),
+    'surname' : new FormControl(null, [Validators.required]),
+    'cpf' : new FormControl(null, [Validators.required, BrazilValidator.isValidCpf]),
+    'cnpj' : new FormControl(null, [BrazilValidator.isValidCpf]),
+    'rg' : new FormControl(null, [Validators.required, BrazilValidator.isValidRG]),
+    'cellphone' : new FormControl(null, [Validators.required]),
+    'companyCellphone' : new FormControl(null),
+    'telephone' : new FormControl(null, [Validators.required]),
+    'companyTelephone' : new FormControl(null),
+    'birthDate' : new FormControl(null, [Validators.required]),
+    'birthDateCompany' : new FormControl(null),
     'subcription' : new FormControl(null),
     'socialReason' : new FormControl(null),
     'fantasyName' : new FormControl(null),
     'ramal' : new FormControl(null),
-    'email' : new FormControl(null),
+    'email' : new FormControl(null, [Validators.required, Validators.email]),
+    'companyEmail' : new FormControl(null, [Validators.email]),
+    'addresses' : new FormArray([])
   })
+
   public enderecos: any = [{}];
 
-  constructor() { }
+  constructor(
+    private readonly clientService : ClientService,
+    private readonly correiosService : CorreiosService,
+    private readonly router : Router
+  ) { }
 
   ngOnInit(): void {
     
   }
 
+  get addresses() {
+    return this.clienteForm.get('addresses') as FormArray;
+  }
+
   public adicionarEndereco(): void {
-    this.enderecos.push({});
+    this.addresses.push(
+      new FormGroup({
+        'cep' : new FormControl(null, [BrazilValidator.isValidCEP]),
+        'address' : new FormControl(null),
+        'number' : new FormControl(null),
+        'complement' : new FormControl(null),
+        'neighborhood' : new FormControl(null),
+        'city' : new FormControl(null),
+        'state' : new FormControl(null),
+        'country' : new FormControl(null),
+      })
+    )
   }
 
   public removerEndereco(id: number): void {
-    (this.enderecos).pop(id)
-    console.log(this.enderecos);
-    
+    this.addresses.removeAt(id)
+    console.log(this.addresses);
   }
 
   public toggleDesativadoCheckbox(): void {
@@ -50,6 +79,93 @@ export class CadastrarClienteComponent implements OnInit {
 
   public toggleTipoPessoa(value: string): void {
     this.tipoPessoa = value;
+    if(value == 'fisica') {
+      // Person validators
+      this.clienteForm.controls.name.setValidators([Validators.required])      
+      this.clienteForm.controls.surname.setValidators([Validators.required])      
+      this.clienteForm.controls.cpf.setValidators([Validators.required, BrazilValidator.isValidCpf])      
+      this.clienteForm.controls.rg.setValidators([Validators.required, BrazilValidator.isValidRG])      
+      this.clienteForm.controls.cellphone.setValidators([Validators.required])      
+      this.clienteForm.controls.telephone.setValidators([Validators.required])      
+      this.clienteForm.controls.birthDate.setValidators([Validators.required])      
+      this.clienteForm.controls.email.setValidators([Validators.required, Validators.email])      
+      // Clear company validators
+      this.clienteForm.controls.fantasyName.clearValidators()
+      this.clienteForm.controls.fantasyName.updateValueAndValidity()
+      this.clienteForm.controls.socialReason.clearValidators()
+      this.clienteForm.controls.socialReason.updateValueAndValidity()
+      this.clienteForm.controls.subcription.clearValidators()
+      this.clienteForm.controls.subcription.updateValueAndValidity()
+      this.clienteForm.controls.cnpj.clearValidators()
+      this.clienteForm.controls.cnpj.updateValueAndValidity()
+      this.clienteForm.controls.companyCellphone.clearValidators()
+      this.clienteForm.controls.companyCellphone.updateValueAndValidity()
+      this.clienteForm.controls.companyTelephone.clearValidators()
+      this.clienteForm.controls.companyTelephone.updateValueAndValidity()
+      this.clienteForm.controls.birthDateCompany.clearValidators()
+      this.clienteForm.controls.birthDateCompany.updateValueAndValidity()
+      this.clienteForm.controls.ramal.clearValidators()
+      this.clienteForm.controls.ramal.updateValueAndValidity()
+      this.clienteForm.controls.companyEmail.clearValidators()
+      this.clienteForm.controls.companyEmail.updateValueAndValidity()
+    } else if(value == 'juridica') {
+      // Company validators
+      this.clienteForm.controls.fantasyName.setValidators([Validators.required])      
+      this.clienteForm.controls.socialReason.setValidators([Validators.required])      
+      this.clienteForm.controls.subcription.setValidators([Validators.required])      
+      this.clienteForm.controls.cnpj.setValidators([Validators.required, BrazilValidator.isValidCpf])      
+      this.clienteForm.controls.companyCellphone.setValidators([Validators.required])      
+      this.clienteForm.controls.companyTelephone.setValidators([Validators.required])      
+      this.clienteForm.controls.birthDateCompany.setValidators([Validators.required])      
+      this.clienteForm.controls.ramal.setValidators([Validators.required])      
+      this.clienteForm.controls.companyEmail.setValidators([Validators.required, Validators.email])
+      //Clear client validators
+      this.clienteForm.controls.name.clearValidators()      
+      this.clienteForm.controls.name.updateValueAndValidity()      
+      this.clienteForm.controls.surname.clearValidators()
+      this.clienteForm.controls.surname.updateValueAndValidity()
+      this.clienteForm.controls.cpf.clearValidators()
+      this.clienteForm.controls.cpf.updateValueAndValidity()
+      this.clienteForm.controls.rg.clearValidators()
+      this.clienteForm.controls.rg.updateValueAndValidity()
+      this.clienteForm.controls.cellphone.clearValidators()
+      this.clienteForm.controls.cellphone.updateValueAndValidity()
+      this.clienteForm.controls.telephone.clearValidators()
+      this.clienteForm.controls.telephone.updateValueAndValidity()
+      this.clienteForm.controls.birthDate.clearValidators()
+      this.clienteForm.controls.birthDate.updateValueAndValidity()
+      this.clienteForm.controls.email.clearValidators()
+      this.clienteForm.controls.email.updateValueAndValidity()
+    }
+  }
+
+  changeAddress(event : any, i : number) {
+    let cep: string = event.target.value;
+    cep = cep.replace('-', '');
+
+    this.correiosService.consultaCep(cep).subscribe((data: any) => {
+      if (data.cep) {
+        (this.clienteForm.controls.addresses as FormArray).at(i).get('address')?.setValue(data.logradouro);
+        (this.clienteForm.controls.addresses as FormArray).at(i).get('neighborhood')?.setValue(data.bairro);
+        (this.clienteForm.controls.addresses as FormArray).at(i).get('city')?.setValue(data.localidade);
+        (this.clienteForm.controls.addresses as FormArray).at(i).get('state')?.setValue(data.uf);
+      }
+    })
+  }
+
+  submitClient(data : any) {
+    if(this.clienteForm.valid) {
+      if (!this.desativadoCheckbox) {
+        data.disabled = true
+      } else {
+        data.disabled = false
+      }
+      this.clientService.create(data).subscribe((dataReturn) => {
+        this.router.navigate(['sistema', 'vendas', 'clientes', 'listar'])
+      }, (err) => {
+        console.log(err)
+      })
+    }
   }
 
 }
