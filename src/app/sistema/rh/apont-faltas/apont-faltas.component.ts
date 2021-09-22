17a910fa5def1a665642faeecae36851963ac2f1
Guiespim <guilherme.monteiro@fingerdigital.com.br>
2021-09-22 09:16:53 -0300

    parte de faltas feita

--- src/app/sistema/rh/apont-faltas/apont-faltas.component.ts
+++ src/app/sistema/rh/apont-faltas/apont-faltas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FaltasService } from 'src/app/services/faltas.service';
 import { RhService } from 'src/app/services/rh.service';
-import { PesquisarColabComponent } from './pesquisar-colab/pesquisar-colab.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-apont-faltas',
@@ -10,18 +11,68 @@ import { PesquisarColabComponent } from './pesquisar-colab/pesquisar-colab.compo
 })
 export class ApontFaltasComponent implements OnInit {
 
+  colabOriginal: any
+  public colab: any[] = []
   public apontForm: FormGroup = new FormGroup({
-    'colaborador': new FormControl(''),
-    'cargo': new FormControl('')
+    'colaborador': new FormControl('', [Validators.required]),
+    'cargo': new FormControl('', [Validators.required]),
+    'data': new FormControl(null, [Validators.required]),
+    'tipo': new FormControl('', [Validators.required])
   })
-  constructor() {}
+  constructor(
+    private faltaService: FaltasService,
+    private rhService: RhService
+  ) {}
   
   ngOnInit(): void {
-    
+    this.faltaService.find().subscribe((data: any)=>{
+      this.colab = data
+      this.colabOriginal = data
+    })
   }
   
   selecionar(data: any){
-    this.apontForm.get('colaborador')?.setValue(`${data.name} ${data.surname}`)
+    this.apontForm.get('colaborador')?.setValue(`${data.name} ${data.surname}`),
+    this.apontForm.get('cargo')?.setValue(data.role)
+  }
+
+  submitForm(data: any){
+    if(this.apontForm.valid){
+      this.faltaService.create(data).subscribe(()=>{
+        this.faltaService.find().subscribe((data: any)=>{
+          this.colab = data
+          console.log(this.colab)
+        })
+      })
+    }else{
+      Swal.fire('Erro', 'Preencha os campos necessários', 'error')
+    }
+  }
+
+  delete(id: number){
+    this.faltaService.delete(id).subscribe(()=>{
+      this.faltaService.find().subscribe((data: any)=>{
+        this.colab = data
+        console.log(this.colab)
+      })
+    })
+  }
+
+  filterBefore = "";
+  filtrar(event : any) {
+    let str = event.target.value;
+    if(str != '') {
+      if(str.length > this.filterBefore.length) {
+        this.colab = this.colabOriginal.filter((user : any) => `${user.colaborador} ${user.cargo} ${user.data.substring(10, 0)} ${user.tipo}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      } else {
+        this.colab = this.colabOriginal;
+        this.colab = this.colabOriginal.filter((user : any) => `${user.name} ${user.surname}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      }
+    } else {
+      this.colab = this.colabOriginal;
+    }
   }
 
 }
--- src/app/sistema/rh/relatorio/relatorio.component.ts
+++ src/app/sistema/rh/relatorio/relatorio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { FaltasService } from 'src/app/services/faltas.service';
+import { RhService } from 'src/app/services/rh.service';
 
 @Component({
   selector: 'app-relatorio',
@@ -8,14 +10,60 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RelatorioComponent implements OnInit {
 
+  colabOriginal: any
+  public rh: any[] = []
+  public colab: any[] = []
   public faltaForm: FormGroup = new FormGroup({
     'data': new FormControl(null),
     'cargo': new FormControl(''),
     'tipoFalta': new FormControl('')
   })
-  constructor() { }
+  constructor(
+    private faltaService: FaltasService,
+    private rhService: RhService
+  ) { }
 
   ngOnInit(): void {
+    this.faltaService.find().subscribe((data: any)=>{
+      this.colab = data
+      this.colabOriginal = data
+    })
+    this.rhService.find().subscribe((data:any)=>{
+      this.rh = data
+    })
+    console.log(this.rh)
+  }
+
+  filterBefore = "";
+  dataFilter(event : any) {
+    let str = event.target.value;
+    if(str != '') {
+      if(str.length > this.filterBefore.length) {
+        this.colab = this.colabOriginal.filter((user : any) => `${user.data.substring(10, 0)}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      } else {
+        this.colab = this.colabOriginal;
+        this.colab = this.colabOriginal.filter((user : any) => `${user.name} ${user.surname}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      }
+    } else {
+      this.colab = this.colabOriginal;
+    }
+  }
+  colabFilter(event: any){
+    let str = event.target.value;
+    if(str != '') {
+      if(str.length > this.filterBefore.length) {
+        this.colab = this.colabOriginal.filter((user : any) => `${user.colaborador}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      } else {
+        this.colab = this.colabOriginal;
+        this.colab = this.colabOriginal.filter((user : any) => `${user.name} ${user.surname}`.toUpperCase().includes(str.toUpperCase()))
+        this.filterBefore = str
+      }
+    } else {
+      this.colab = this.colabOriginal;
+    }
   }
 
 }
