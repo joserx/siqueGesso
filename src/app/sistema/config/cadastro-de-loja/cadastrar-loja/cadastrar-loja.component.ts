8db13e3b1d7681429591bb9f4918af6c2c83691b
brunafinger <bruna.grassi@fingerdigital.com.br>
2021-12-13 12:25:55 -0300

    campo adicionar banner

--- src/app/sistema/config/cadastro-de-loja/cadastrar-loja/cadastrar-loja.component.ts
+++ src/app/sistema/config/cadastro-de-loja/cadastrar-loja/cadastrar-loja.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-loja.component.scss']
 })
 export class CadastrarLojaComponent implements OnInit {
+  url: any;
+  text = 'Adicionar banner';
+  status = 'Salvar'
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  uploadImage(event: any) {
+    let label = event.target.closest('.upload__image');
+    let imgDefault = label.querySelector('.upload__image--img') as HTMLImageElement;
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.url = reader.result; 
+        imgDefault.src = this.url;
+        this.text = 'Alterar imagem'
+      };
+    }
   }
 
 }
