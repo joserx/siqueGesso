7de75e5cc296076972a23e1312737b6e41deeb93
joserx <jose.hamilton2050@gmail.com>
2022-01-24 00:40:19 -0300

    alterações categoria de produtos

--- src/app/sistema/config/cadastro-de-categoria/cadastro-de-produto-lista/cadastro-de-produto-lista.component.ts
+++ src/app/sistema/config/cadastro-de-categoria/cadastro-de-produto-lista/cadastro-de-produto-lista.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaProdutoService } from 'src/app/services/categoria-produto.service';
 
 @Component({
   selector: 'app-cadastro-de-produto-lista',
   templateUrl: './cadastro-de-produto-lista.component.html',
-  styleUrls: ['./cadastro-de-produto-lista.component.scss']
+  styleUrls: ['./cadastro-de-produto-lista.component.scss'],
 })
 export class CadastroDeProdutoListaComponent implements OnInit {
-  produtos = [
-    {},{},{},{},{},
-  ]
+  public categoria: any;
+  public categorias: any;
 
-  constructor() { }
+  constructor(private CategoriaProdutoService: CategoriaProdutoService) {}
 
   ngOnInit(): void {
+    this.getCategorias();
   }
 
+  getCategorias() {
+    this.CategoriaProdutoService.find().subscribe((res) => {
+      this.CategoriaProdutoService.categoria = res;
+      this.categoria = res;
+      this.categorias = this.categoria;
+      console.log(res);
+    });
+  }
 }
--- src/app/sistema/config/cadastro-de-categoria/cadastro-de-produto/cadastro-de-produto.component.ts
+++ src/app/sistema/config/cadastro-de-categoria/cadastro-de-produto/cadastro-de-produto.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormArray,
+  FormGroup,
+  FormControlName,
+} from '@angular/forms';
+
+import Swal from 'sweetalert2';
+
+import { CategoriaProdutoService } from 'src/app/services/categoria-produto.service';
 
 @Component({
   selector: 'app-cadastro-de-produto',
   templateUrl: './cadastro-de-produto.component.html',
-  styleUrls: ['./cadastro-de-produto.component.scss']
+  styleUrls: ['./cadastro-de-produto.component.scss'],
 })
 export class CadastroDeProdutoComponent implements OnInit {
+  categoriaForm = new FormGroup({
+    nome: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private CategoriaProdutoService: CategoriaProdutoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit(): any {
+    this.CategoriaProdutoService.create(this.categoriaForm.value).subscribe(
+      () => {
+        // this.reload.emit();
+        // this.closeBtn.nativeElement.click();
+        this.categoriaForm.reset();
+        return Swal.fire({
+          title: 'Produto salvo!',
+          icon: 'success',
+          toast: true,
+          position: 'top',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+      }
+    );
   }
-
 }
