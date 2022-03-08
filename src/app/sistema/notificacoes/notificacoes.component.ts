f8b53426b2f8140bdf206c71d4c3e70ac3eeae79
yan <yan.carlos@fingerdigital.com.br>
2022-03-08 17:15:41 -0300

    Adiciona a modal da página de mensagens

--- src/app/sistema/notificacoes/notificacoes.component.ts
+++ src/app/sistema/notificacoes/notificacoes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MensagemService} from 'src/app/services/mensagem.service'
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-notificacoes',
   templateUrl: './notificacoes.component.html',
@@ -20,7 +21,28 @@ export class NotificacoesComponent implements OnInit {
   getMessages(){
     this.mensagemService.find().subscribe((res)=>{
       this.mensagemService.notifications = res;
+      console.log(res)
       this.mensagens = res
     })
   }
+
+  viewMessage(mensagem: any){
+    console.log(mensagem);
+  }
+
+  deleteMessage(mensagem: any){
+    this.mensagemService.delete(mensagem.id).subscribe(() => {
+      this.getMessages();
+      return Swal.fire({
+        title: 'Mensagem Deletada!',
+        icon: 'success',
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+      });
+    })
+  }
+
 }
