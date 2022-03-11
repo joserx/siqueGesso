b8142b8e501c3dd1d9260b2c88f3fdb31da37845
Guiespim <guilherme.monteiro@fingerdigital.com.br>
2022-03-11 14:55:09 -0300

    permission system

--- src/app/sistema/config/permissoes/criar-permissoes/criar-permissoes.component.ts
+++ src/app/sistema/config/permissoes/criar-permissoes/criar-permissoes.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+import { PermissionsUsers } from 'src/app/services/permissions/permissions';
 @Component({
   selector: 'app-criar-permissoes',
   templateUrl: './criar-permissoes.component.html',
@@ -8,27 +8,47 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class CriarPermissoesComponent implements OnInit {
 
   @ViewChild('opt') opt: ElementRef;
+  permissions: any = []
   areas: any = [
     {
-      name: 'VENDER'
+      name: 'VENDAS',
+      edit: PermissionsUsers.vendas_editar,
+      delete: PermissionsUsers.vendas_excluir,
+      view: PermissionsUsers.vendas_ver,
     },
     {
-      name: 'COMPRAS'
+      name: 'COMPRAS',
+      edit: PermissionsUsers.compras_editar,
+      delete: PermissionsUsers.compras_excluir,
+      view: PermissionsUsers.vendas_ver,
     },
     {
-      name: 'ESTOQUE'
+      name: 'ESTOQUE',
+      edit: PermissionsUsers.estoque_editar,
+      delete: PermissionsUsers.estoque_excluir,
+      view: PermissionsUsers.estoque_ver,
     },
     {
-      name:'FINANCEIRO'
+      name:'FINANCEIRO',
+      edit: PermissionsUsers.financeiro_editar,
+      delete: PermissionsUsers.financeiro_excluir,
+      view: PermissionsUsers.financeiro_ver,
     },
     {
-      name: 'EXPEDIÇÃO'
+      name: 'EXPEDIÇÃO',
+      edit: PermissionsUsers.expedicao_editar,
+      delete: PermissionsUsers.expedicao_excluir,
+      view: PermissionsUsers.expedicao_ver,
     },
     {
-      name: 'RH'
+      name: 'RH',
+      edit: PermissionsUsers.rh_editar,
+      delete: PermissionsUsers.rh_excluir,
+      view: PermissionsUsers.rh_ver,
     }
   ]
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
   }
@@ -36,5 +56,18 @@ export class CriarPermissoesComponent implements OnInit {
   closeOpt(){
     this.opt.nativeElement.click()
   }
+
+  addPermission(value: string, event: any){
+    let input = event
+    if(input.target.checked){
+      if(this.permissions.indexOf(value)==-1){
+        this.permissions.push(value)
+      }
+    }else{
+      this.permissions.splice(this.permissions.indexOf(value), 1)
+    }
+    console.log(this.permissions)
+  }
+
 
 }
